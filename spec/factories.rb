d27d7140d6dbce941a092f03c3f6d04eb32e9b38
Mat-Rix <mat.rix@wp.pl>
2013-05-13 18:27:28 +0200

    Added a User factory and tests for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,10 @@
+
+Factory.define :user do |user|
+  user.name                  "Lechu"
+  user.email                 "user@company.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
+
+
+
